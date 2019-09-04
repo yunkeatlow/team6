3bce862ffd6c9fd6e4dc46d70e2844504cb2e3f9
gangSTAR-pro <54874464+gangSTAR-pro@users.noreply.github.com>
2019-09-04 10:36:04 +0800

    Update ToDoList.java

--- src/src/ToDoList.java
+++ src/src/ToDoList.java
@@ -1,35 +1,42 @@
-package src;
-
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 
 public class ToDoList {
-private HashMap<String, Task> tasks = new HashMap<String, Task>();
 
+	private HashMap<String, Task> tasks = new HashMap<String, Task>();
+	
 	public void addTask (Task task) {
-		//Add code here
+		tasks.put(task.getDescription(), task);
 	}
 	public void completeTask(String description) {
-		// Add code here
+		Task task = null;
+		if ((task = tasks.get(description)) != null){
+			task.setComplete(true);
+		};
 	}
 	public boolean getStatus(String description) {
-		//Add code here
+		Task task = null;
+		if ((task = tasks.get(description)) != null){
+			return task.isComplete();
+		};
 		return false;
 	}
 	public Task getTask(String description) {
-		// Add code here
-		return null;
+		return tasks.get(description);
 	}
 	public Task removeTask(String description) {
-		// Add code here
-		return null;
+		return tasks.remove(description);
 	}
 	public Collection<Task> getAllTasks() {
 		return tasks.values();
 	}
 	public Collection<Task> getCompletedTasks() {
-		// Add code here
-		return null;
+		Collection<Task> completedTasks = new ArrayList<Task> ();
+		Collection<Task> allTasks = new ArrayList<Task> ();
+		allTasks = getAllTasks();
+		for (Task task: allTasks) 
+			if (task.isComplete() == true) completedTasks.add(task);
+		return completedTasks;
 	}
 }
